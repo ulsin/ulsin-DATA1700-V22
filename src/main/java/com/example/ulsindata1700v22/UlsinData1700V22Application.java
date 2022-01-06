d721dae681244d6fe85d61e8b3986157498fd72e
Soyboiii <ulriksin@gmail.com>
2022-01-06 11:32:14 +0100

    Første commit, committer alt, fjener kanskje senere

---
+++ src/main/java/com/example/ulsindata1700v22/UlsinData1700V22Application.java
@@ -0,0 +1,15 @@
+package com.example.ulsindata1700v22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlsinData1700V22Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hey!");
+
+        SpringApplication.run(UlsinData1700V22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ulsindata1700v22/UlsinData1700V22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ulsindata1700v22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlsinData1700V22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
